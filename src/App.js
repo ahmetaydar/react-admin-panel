67a3facd18cf00672ea5c98c2020486f08f7a9d5
ahmetaydar <a.aydar2014@gmail.com>
2023-07-04 23:24:17 +0300

    added manage team component and header component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { ColorModeContext, useMode } from "./theme";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar.jsx";
 import Sidebar from "./scenes/global/Sidebar";
-// import Dashboard from ".'scenes/dashboard";
-// import Team from "./scenes/team";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -27,9 +27,9 @@ function App() {
           <main className="content">
             <Topbar />
             <Routes>
-              {/* <Route path="/" element={<Dashboard />} /> */}
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              {/*} <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -81,7 +81,7 @@ const Sidebar = () => {
                 ml="15px"
               >
                 <Typography variant="h3" color={colors.grey[100]}>
-                  ADMINS
+                  ADMINIS
                 </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
